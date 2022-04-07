1c4d6e2a450efdf05378fbbe326713785b3bd2cc
Samuel Dennis <sdhasjim@gmail.com>
2022-04-07 11:35:00 +0700

    updated Trape project 07/04

---
+++ MC1-Galas-Trape/component/screen-view/DetailList/DetailListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailListViewController.swift
+//  galas
+//
+//  Created by Samuel Dennis on 04/04/22.
+//
+
+import UIKit
+
+class DetailListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // develop fariz -> kerja dulu -> commit push ke branch develop-fariz -> PR ke main -> (kalo ada konflik rebase dulu dari) -> merged di main -> local di main dipull -> branch develop fariz
+
+}
---
+++ MC1-Galas-Trape/component/screen-view/SelectActivities/SelectActivitiesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectActivitiesViewController.swift
+//  galas
+//
+//  Created by Renzo Alvaroshan on 04/04/22.
+//
+
+import UIKit
+
+class SelectActivitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
