b5b06d56613840ba9f3a5b7caed976ac61c3e93e
Samuel Dennis <sdhasjim@gmail.com>
2022-04-07 11:19:22 +0700

    Initial Commit

---
+++ MC1-Galas-Trape/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MC1-Galas-Trape
+//
+//  Created by Samuel Dennis on 07/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
