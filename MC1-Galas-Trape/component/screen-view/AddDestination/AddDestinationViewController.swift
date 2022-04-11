c58913246180519803ad81d7ebfbc4a115d7c577
Fariz Rizky Rizaldy <farizrizkyrizaldy@gmail.com>
2022-04-11 15:22:24 +0700

    add functionality

--- MC1-Galas-Trape/component/screen-view/AddDestination/AddDestinationViewController.swift
+++ MC1-Galas-Trape/component/screen-view/AddDestination/AddDestinationViewController.swift
@@ -12,13 +12,21 @@ class AddDestinationViewController: UIViewController {
     @IBOutlet weak var countingLabel: UILabel!
     @IBOutlet weak var startDatePicker: UIDatePicker!
     @IBOutlet weak var endDatePicker: UIDatePicker!
-    var timer = TimeInterval()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        filteredData = data
 //        timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: "updateCounter", userInfo: nil, repeats: true)
 
         // Do any additional setup after loading the view.
-        updateCounter()
+//        if (endDatePicker != nil) {
+//            updateCounter()
+//        }
+//        tblview.dataSource = self
+//        searchBar.delegate = self
+//        filteredData = data
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -26,22 +34,35 @@ class AddDestinationViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func selectDate(_ sender: Any) {
+    @IBAction func insertEnd(_ sender: Any) {
+        updateCounter()
     }
     
-    @IBAction func selectList(_ sender: Any) {
-        
+    @IBAction func insertStart(_ sender: Any) {
+        updateCounter()
     }
     
     func updateCounter() {
         let timeLeft = endDatePicker.date.timeIntervalSinceNow - startDatePicker.date.timeIntervalSinceNow
         countingLabel.text = timeLeft.time
     }
+    
+//    @objc(tableView:cellForRowAtIndexPath:) func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath) as UITableViewCell
+//        cell.textLabel?.text = filteredData[indexPath.row]
+//        return cell
+//    }
+//
+//    @objc func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return filteredData.count
+//    }
 
 }
 
 extension TimeInterval {
     var time:String {
-        return String(format:"%02dd %02dh %02dm %02ds", Int((self/86400)), Int((self/3600.0)), Int((self/60.0)), Int((self)))
+        return String(format:"%2d", Int((self/86400)+1), Int((self/3600.0)), Int((self/60.0)), Int((self)))
     }
+    
+//    %02dh %02dm %02ds
 }
