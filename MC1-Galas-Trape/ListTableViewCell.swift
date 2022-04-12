d4ec9bbcc2a33b16e7e575b7430a15b9ca2f97d3
Renzo Alvaroshan <renzo.alvaroshan@gmail.com>
2022-04-12 08:01:26 +0700

    Major update Select Activities FUnctionality branch

---
+++ MC1-Galas-Trape/ListTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ListTableViewCell.swift
+//  MC1-Galas-Trape
+//
+//  Created by Renzo Alvaroshan on 11/04/22.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var checkBoxButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBAction func checkToggled(_ sender: UIButton) {
+        
+    }
+}
---
+++ MC1-Galas-Trape/ToDoListViewController.swift
@@ -0,0 +1,143 @@
+//
+//  ToDoListViewController.swift
+//  MC1-Galas-Trape
+//
+//  Created by Renzo Alvaroshan on 11/04/22.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    @IBOutlet weak var tableView: UITableView?
+    
+//    let tableView: UITableView = {
+//        let table = UITableView()
+//        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+//        return table
+//    }()
+    
+    var models = [ToDoListItem]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "To Do List Core Data"
+//        view.addSubview(tableView)
+        getAllItems()
+        tableView?.delegate = self
+        tableView?.dataSource = self
+        tableView?.frame = view.bounds
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ListTableViewCell
+        cell.nameLabel.text = model.name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = models[indexPath.row]
+        let sheet = UIAlertController(title: "Edit", message: nil, preferredStyle: .actionSheet)
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { _ in
+
+            let alert = UIAlertController(title: "Edit Item", message: "Edit Your Item", preferredStyle: .alert)
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else { return }
+
+                self?.updateItem(item: item, newName: newName)
+            }))
+
+            self.present(alert, animated: true)
+        }))
+
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+
+        present(sheet, animated: true)
+    }
+    
+    @objc private func didTapAdd() {
+        let alert = UIAlertController(title: "New Item", message: "Enter New Item", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
+            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else { return }
+            
+            self?.createItem(name: text)
+        }))
+        
+        present(alert, animated: true)
+    }
+    
+    // Core Data
+    
+    func getAllItems() {
+        do {
+            models = try context.fetch(ToDoListItem.fetchRequest())
+            DispatchQueue.main.async {
+                self.tableView?.reloadData()
+            }
+        } catch {
+            print("Error")
+        }
+    }
+    
+    func createItem(name: String) {
+        let newItem = ToDoListItem(context: context)
+        newItem.name = name
+        newItem.completed = false
+        
+        do {
+            try context.save()
+            getAllItems()
+        } catch {
+            print("Error")
+        }
+    }
+    
+    func deleteItem(item: ToDoListItem) {
+        context.delete(item)
+        
+        do {
+            try context.save()
+            getAllItems()
+        } catch {
+            print("Error")
+        }
+    }
+    
+    func updateItem(item: ToDoListItem, newName: String) {
+        item.name = newName
+        
+        do {
+            try context.save()
+            getAllItems()
+        } catch {
+            print("Error")
+        }
+    }
+    
+}
+
+//extension ToDoListViewController: ListTableViewCellDelegate {
+//    
+//    func checkBoxToggle(sender: ListTableViewCell) {
+//        if let selectedIndexPath = tableView?.indexPath(for: sender) {
+//            models[selectedIndexPath.row].completed = !models[selectedIndexPath.row].completed
+//            tableView?.reloadRows(at: [selectedIndexPath], with: .automatic)
+//        }
+//    }
+//}
