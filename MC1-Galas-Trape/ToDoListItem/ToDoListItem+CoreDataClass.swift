ef3b6235c8a02d9bac94c852bf42ef6f48c277a8
Samuel Dennis <sdhasjim@gmail.com>
2022-04-12 10:32:49 +0700

    Revert "renzo code"
    This reverts commit 192cd40316931a5822e96ba80141f48963f35b17.

---
+++ MC1-Galas-Trape/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  MC1-Galas-Trape
+//
+//  Created by Renzo Alvaroshan on 11/04/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
